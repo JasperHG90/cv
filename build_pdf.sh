66aa9c1f97be7a6c12ba5cb144299d81d0223844
Jasper Ginn <jasperginn@gmail.com>
2020-01-14 13:06:13 +0100

    Update PDF

--- build_pdf.sh
+++ build_pdf.sh
@@ -3,12 +3,7 @@
 # Store printed CV file here
 OUTFILE="$PWD/JHGINN_CV.pdf"
 
-## Build and serve jekyll site
-#jekyll serve #> /dev/null 2>&1 &
-
 # Convert cv html to pdf
 # - Viewport sets the 'size' of the screen that is visiting the website
-wkhtmltopdf --javascript-delay 10000 --viewport-size 1280x1024 https://jasperhg90.github.io/cv/ $OUTFILE
+wkhtmltopdf --javascript-delay 10000 --viewport-size 1280x1024 --margin-bottom 10mm --margin-top 10mm --page-size A4 https://jasperhg90.github.io/cv/ $OUTFILE
 
-# Kill server
-#ps aux |grep jekyll |awk '{print $2}' | xargs kill -9
